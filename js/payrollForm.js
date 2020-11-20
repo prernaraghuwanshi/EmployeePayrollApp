180e27f063abb27b171654194e93bca2cca236e8
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-11-20 12:19:17 +0530

    [Prerna]ADDED: Reset employee form on reset

--- js/payrollForm.js
+++ js/payrollForm.js
@@ -67,6 +67,20 @@ const save = () => {
   }
 }
 
+const resetForm = () => {
+  setValue('#name', '');
+  setTextValue('.text-error', "");
+  unsetSelectedValues('[name=profile]');
+  unsetSelectedValues('[name=gender]');
+  unsetSelectedValues('[name=department]');
+  setValue('#salary', '');
+  setValue('#notes', '');
+  setValue('#day', '01');
+  setValue('#month', '0');
+  setValue('#year', '2020');
+  setTextValue('.date-error', '');
+}
+
 const createEmployeePayroll = () => {
   let employeePayrollData = new EmployeePayrollData();
   try {
@@ -108,4 +122,21 @@ const getSelectedValues = (propertyValue) => {
       selItems.push(item.value);
   });
   return selItems;
+}
+
+const unsetSelectedValues = (propertyValue) => {
+  let allItems = document.querySelectorAll(propertyValue);
+  allItems.forEach(item => {
+    item.checked = false;
+  });
+}
+
+const setTextValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.textContent = value;
+}
+
+const setValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.value = value;
 }
